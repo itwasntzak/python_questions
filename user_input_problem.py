d81c7d1b90ebbd13932b28ac57e4f400233022ab
itwasntzak <51720888+itwasntzak@users.noreply.github.com>
2020-09-30 20:00:48 -0600

    Why won't this work?

---
+++ user_input_problem.py
@@ -0,0 +1,97 @@
+def user_input(prompt, preced=None):
+    '''
+    prompt is displayed to user before they input anything, must be a string
+    preced is on the same line before the users input, must be a string
+    '''
+    # not unit testible, requires user input
+    print(prompt, end='')
+    if preced:
+        return input(preced)
+    return input()
+    
+
+def check_text(user_input, spaces=False, symbols=None):
+    '''
+    symbols needs to be a string of characters to allow the user to enter
+    '''
+    if symbols and not isinstance(symbols, str):
+        raise TypeError
+
+    import re
+
+    if spaces is True and symbols:
+        pattern = r'^[a-zA-Z ' + symbols + ']$'
+    elif symbols:
+        pattern = r'^[a-zA-Z' + symbols + ']$'
+    elif spaces is True:
+        pattern = r'^[a-zA-Z ]$'
+    else:
+        pattern = r'^[a-zA-Z]$'
+
+    for character in user_input:
+        if not re.match(pattern, character):
+            return False
+
+    return True
+
+
+def input_text(prompt, spaces=False, symbols=None):
+    if symbols and not isinstance(symbols, str):
+        raise TypeError
+
+    error_message = '\nError, please enter alpha characters'
+    if spaces is True and symbols:
+        error_message += f', spaces, or these {symbols}'
+    elif spaces is True:
+        error_message += ' or spaces'
+    elif symbols:
+        error_message += f' or these {symbols}'
+
+    text = user_input(prompt)
+    while not check_text(text, spaces, symbols):
+        print(error_message)
+        text = user_input(prompt)
+    
+    return text
+
+
+def check_confirm(user_input):
+    '''
+    checks that the user has input y or n to confirm different things
+    '''
+    if not isinstance(user_input, str):
+        raise TypeError(f'user_input needs to be a string not {type(user_input)}')
+
+    import re
+    if re.match('^[y]$', user_input, flags=re.IGNORECASE):
+        return True
+    elif re.match('^[n]$', user_input, flags=re.IGNORECASE):
+        return False
+
+
+def confirm(data_string):
+    import re
+    from resources.strings import confirm_text
+    from utility.utility import add_newlines
+
+    prompt = add_newlines(data_string) + confirm_text
+    prompt = add_newlines(prompt)
+
+    user_choice = user_input(prompt)
+    while not re.match('^[yn]$', user_choice, flags=re.IGNORECASE):
+        print('\nError, please enter Y for yes or N for no')
+        user_choice = user_input(prompt)
+
+    return check_confirm(user_choice)
+
+
+class User_Input():
+    def __init__(self, prompt):
+        self.prompt = prompt
+
+    def location(self):
+        location = input_text(self.prompt, spaces=True, symbols=',./&?!:;()[]-+%')
+        while not confirm(location):
+            location = input_text(self.prompt, spaces=True, symbols=',./&?!:;()[]-+%')
+
+        return location
